7246c5509fb977f2e3ac116cf2bf4eba46bb9814
Komal-Prajapat <komalprajapat267@gmail.com>
2024-09-03 20:04:26 +0530

    Result Page

--- src/pages/Result/Index.jsx
+++ src/pages/Result/Index.jsx
@@ -4,43 +4,56 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import QuizButton from './../../components/quizButton/Index';
 
 const Result = () => {
- const navigate = useNavigate();
+  const navigate = useNavigate();
   const location = useLocation();
-  const { score, total } = location.state || {}; 
+  const { score, total } = location.state || {};
   const startQuiz = (category) => {
-    console.log(category,"category");
+    console.log(category, "category");
     navigate(`/quiz/${category}`);
   };
 
   return (
     <div>
       <Container>
-        <h1>Well Played! You scored {score} out of {total}</h1>
+        <h1
+          style={{
+            width: '95%', textAlign: 'center', fontSize: '30px', fontWeight: 'bolder', justifyContent: "center"
+          }}
+        >Well Played! You scored {score} out of {total}</h1>
         <Grid container justifyContent="center" alignItems="center" sx={{ my: 3 }}>
           <Box className="outerdiv">
             <Box className="innerdiv"></Box>
           </Box>
         </Grid>
 
-<p>BAsed on Your performance , yoyu get a 5 % difcount coupon to shopwith us.</p>
-<h3>Coupon code : QUIZ766</h3>
+        <p style={{
+          textAlign: 'center',
+          fontSize: '20px',
+        }}>Based on Your performance , yoyu get a 5 % difcount coupon to shopwith us.
+          <span> <h4>Coupon code : QUIZ766</h4>
+          </span>
+        </p>
 
-<Link>
-SHOP NOW 
-</Link>
-<Grid container justifyContent="center" alignItems="center" sx={{ my: 3 }}>
+        <Link >
+         <span style={{
+          textAlign: 'center',
+          fontSize: '20px',
+          color:"var(--black-color)",
+        }} > SHOP NOW</span>
+        </Link>
+        <Grid container justifyContent="center" alignItems="center" sx={{ my: 3 }}>
           <Box className="outerdiv">
             <Box className="innerdiv"></Box>
           </Box>
         </Grid>
         <h3>or , you can countinuw playing our quizzes , select a category from below to start playing again:</h3>
         <Grid container spacing={2} sx={{ mx: 0.1 }}>
-        <Grid item xs={5.2} sm={5.2} md={5.2} onClick={() => startQuiz('indian-army')}>
-          <QuizButton name="Indian Army"  />
-        </Grid>
-        <Grid item xs={5.2} sm={5.2} md={5.2} onClick={() => startQuiz('indian-navy')}>
-          <QuizButton name="Indian Navy"  />
-        </Grid>
+          <Grid item xs={5.2} sm={5.2} md={5.2} onClick={() => startQuiz('indian-army')}>
+            <QuizButton name="Indian Army" />
+          </Grid>
+          <Grid item xs={5.2} sm={5.2} md={5.2} onClick={() => startQuiz('indian-navy')}>
+            <QuizButton name="Indian Navy" />
+          </Grid>
         </Grid>
       </Container>
     </div>
