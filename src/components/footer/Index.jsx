ad4b79c633a7915b495d456a7c628a4bdcda70c8
Komal-Prajapat <komalprajapat267@gmail.com>
2024-09-04 13:30:46 +0530

    update

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import './index.css';
-import Navbar from '../Navbar';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { Box } from "@mui/material";
+import "./index.css";
+import Navbar from "../Navbar";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
   const location = useLocation();
-  const showNavLink = location.pathname === '/' || location.pathname === '/startquiz' || location.pathname === '/results';
+  const showNavLink =
+    location.pathname === "/" ||
+    location.pathname === "/startquiz" ||
+    location.pathname === "/results";
 
   return (
     <>
-
       <Box
         sx={{
           clipPath: "polygon(0 43%, 100% 0, 100% 100%, 0% 100%)",
-          backgroundColor: 'var(--footer-color)',
-          height: '300px',
-          width: '400px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          backgroundColor: "var(--footer-color)",
+          height: "300px",
+          // width: "400px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           // position: 'relative',
           // bottom: '0',
-          zIndex: '-1',
-
+          zIndex: "-1",
         }}
         className="footer"
       >
         {showNavLink && <Navbar></Navbar>}
-        {!showNavLink && <p
-          style={{
-            marginTop: "auto",
-            textAlign: 'center',
+        {!showNavLink && (
+          <p
+            style={{
+              marginTop: "auto",
+              textAlign: "center",
 
-            width: '400px',
-          }}
-        >
-          Each quiz has 5 questions
-        </p>}
+              width: "400px",
+            }}
+          >
+            Each quiz has 5 questions
+          </p>
+        )}
       </Box>
-
     </>
   );
 };
